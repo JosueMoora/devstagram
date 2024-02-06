a37f3c1b107d29178b6c5ad748d64052c55f7d4c
JosueMoora <josuedavidmora99@gmail.com>
2024-02-06 01:20:24 -0400

    Agrega configuración de Image::configure() para usar Imagick en lugar de GD

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -14,6 +14,7 @@ class ImagenController extends Controller
         $imagen = $request->file('file');
 
         $nombreImagen = Str::uuid() . "." . $imagen->extension();
+        $imagenServidor = Image::configure(['driver' => 'imagick']);
         $imagenServidor = Image::make($imagen);
         $imagenServidor->fit(1000, 1000);
 
